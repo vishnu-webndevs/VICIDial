5b1dad3ea14a0ec60b568a7f5e64a0965d83efeb
vishnu_devs <vishnudevs9413@gmail.com>
2026-06-24 16:39:31 +0530

    Fix: Remove invalid headers argument from trustProxies

--- apps/api/bootstrap/app.php
+++ apps/api/bootstrap/app.php
@@ -46,15 +46,8 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         $middleware->redirectGuestsTo('/login');
 
-        // Trust all proxies and set HTTPS
+        // Trust all proxies
         $middleware->trustProxies(at: '*');
-        $middleware->trustProxies(headers: [
-            Request::HEADER_X_FORWARDED_FOR,
-            Request::HEADER_X_FORWARDED_HOST,
-            Request::HEADER_X_FORWARDED_PORT,
-            Request::HEADER_X_FORWARDED_PROTO,
-            Request::HEADER_X_FORWARDED_AWS_ELB,
-        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (AuthenticationException $exception, Request $request) {
